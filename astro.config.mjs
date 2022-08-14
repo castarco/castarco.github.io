bdb60f6f5e998bfd669601dedc0177812014dd20
Andres Correa Casablanca <castarco@coderspirit.xyz>
2022-08-14 10:49:22 +0200

    feat: enable LaTeX & add some old posts

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,21 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 import mdx from "@astrojs/mdx";
 import solid from "@astrojs/solid-js";
 import tailwind from "@astrojs/tailwind";
 
+import remarkMath from "remark-math";
+import rehypeKatex from "rehype-katex";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx(), solid(), tailwind()],
-  site: `http://astro.build`
+	integrations: [
+		mdx({
+			remarkPlugins: [remarkMath],
+			rehypePlugins: [rehypeKatex],
+		}),
+		solid(),
+		tailwind(),
+	],
+	site: `http://blog.coderspirit.xyz`,
 });
